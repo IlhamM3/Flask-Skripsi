5c80eb4ecc994132cacde34367ae2121988971cb
ilhamM3 <ilhammaulana05303005@gmail.com>
2025-02-21 07:18:41 +0700

    change config

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Pastikan menggunakan Python 3.10 jika tersedia
-if command -v pyenv 1>/dev/null 2>&1; then
-    pyenv install 3.10.12 -s
-    pyenv global 3.10.12
-fi
+# pyenv install 3.10.12 -s
+# pyenv global 3.10.12
 
 # Install dependencies
 pip install -r requirements.txt
